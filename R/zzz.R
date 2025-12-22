56dab32f130308228f636017f6f5d2c93ae384f3
singhdevang111 <147700801+singhdevang111@users.noreply.github.com>
2025-12-22 13:24:27 +0000

    Added msg function to tell user they are one the latest version

--- R/zzz.R
+++ R/zzz.R
@@ -1,26 +1,37 @@
 .onAttach <- function(libname, pkgname) {
   local_version <- utils::packageVersion("lookups")
 
-  # Use suppressWarnings so the user doesn't see "404 Not Found" or connection errors
+  # 1. Try to get the latest version from GitHub
   latest_version <- tryCatch({
     url <- "https://raw.githubusercontent.com/singhdevang111/lookups/main/DESCRIPTION"
 
-    # suppressWarnings hides the connection issues from the user
+    # suppressWarnings hides the connection issues/404s from the user
     desc <- suppressWarnings(readLines(url, warn = FALSE))
 
     v_line <- desc[grep("^Version:", desc)]
-    gsub("Version: ", "", v_line)
+    if(length(v_line) > 0) gsub("Version: ", "", v_line) else NULL
   }, error = function(e) NULL)
 
-  if (!is.null(latest_version) && local_version < latest_version) {
-    packageStartupMessage(paste0(
-      "\n",
-      "****************************************************************\n",
-      "  A NEW VERSION of 'lookups' (v", latest_version, ") is available!\n",
-      "  Your version: ", local_version, "\n\n",
-      "  Please run the following to update:\n",
-      "  devtools::install_github('singhdevang111/lookups')\n",
-      "****************************************************************\n"
-    ))
+  # 2. Provide the appropriate feedback message
+  if (!is.null(latest_version)) {
+    if (local_version < latest_version) {
+      # Scenario: Update required
+      packageStartupMessage(paste0(
+        "\n",
+        "****************************************************************\n",
+        "  UPDATE AVAILABLE: A newer version of 'lookups' (v", latest_version, ") exists!\n",
+        "  Your version: ", local_version, "\n",
+        "  Run devtools::install_github('singhdevang111/lookups') to update.\n",
+        "****************************************************************\n"
+      ))
+    } else {
+      # Scenario: Up to date (This is what you want to see every time)
+      packageStartupMessage(paste0(
+        "Checking for updates... You are using the latest version of 'lookups' (v", local_version, ")."
+      ))
+    }
+  } else {
+    # Optional: Offline or GitHub unavailable message
+    packageStartupMessage(paste0("Loaded 'lookups' v", local_version, " (Offline mode)."))
   }
 }
